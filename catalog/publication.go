1720ce058eba1fa09c22d23cc3c883f639d3fe27
Tanay Soni <tanaysoni12@gmail.com>
2018-04-15 19:15:06 +0530

    code formatting

--- catalog/publication.go
+++ catalog/publication.go
@@ -1,8 +1,8 @@
 package catalog
 
 type Publication struct {
-	PublicationId int `gorm:"primary_key"`
-	PublicationFrequency int8 // in weeks
-	Publisher string `gorm:"size:128"`
-	Genre string `gorm:"size:128"`
+	PublicationId        int    `gorm:"primary_key"`
+	PublicationFrequency int8   // in weeks
+	Publisher            string `gorm:"size:128"`
+	Genre                string `gorm:"size:128"`
 }
