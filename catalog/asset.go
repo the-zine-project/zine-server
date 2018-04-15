a75978d206fc1ed2938003b0b6b9d82fc31b58ff
Tanay Soni <tanaysoni12@gmail.com>
2018-04-15 19:15:21 +0530

    add son tags

--- catalog/asset.go
+++ catalog/asset.go
@@ -1,12 +1,8 @@
 package catalog
 
 type Asset struct {
-	AssetId int `gorm:"primary_key"`
-	AssetType int `gorm:"default:1"` // default is magazine(type:1)
-
-	MagazineId int `gorm:"type:bigint REFERENCES magazines(magazine_id)"`
+	AssetId   int `gorm:"primary_key" json:"id"`
+	AssetType int `gorm:"default:1" json:"type"` // default is magazine(type:1)
+	MagazineId int `gorm:"type:bigint REFERENCES magazines(magazine_id)" json:"-"`
 }
-
-
-
-
+gt
