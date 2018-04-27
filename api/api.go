7c0a6ad86117c10d1321991baab8e9e410243037
Tanay Soni <tanaysoni12@gmail.com>
2018-04-27 19:34:13 +0530

    move graphql artifacts within the corresponding package

--- api/api.go
+++ api/api.go
@@ -15,41 +15,10 @@ type graphqlBody struct {
 	Query string
 }
 
-var assetType = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "Asset",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"type": &graphql.Field{
-				Type: graphql.Int,
-			},
-		},
-	},
-)
-
 var queryType = graphql.NewObject(
 	graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			"assets": &graphql.Field{
-				Type: graphql.NewList(assetType),
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.Int,
-					},
-					"type": &graphql.ArgumentConfig{
-						Type: graphql.Int,
-					},
-				},
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					var assets []catalog.Asset
-					db.DBCon.Select([]string{"asset_id, asset_type"}).Find(&assets)
-					return assets, nil
-				},
-			},
-		},
+		Name:   "Query",
+		Fields: catalog.AssetsField,
 	})
 
 var schema, _ = graphql.NewSchema(
--- catalog/asset.go
+++ catalog/asset.go
@@ -1,7 +1,45 @@
 package catalog
 
+import (
+	"github.com/graphql-go/graphql"
+	"zine/db"
+)
+
 type Asset struct {
-	AssetId   int `gorm:"primary_key" json:"id"`
-	AssetType int `gorm:"default:1" json:"type"` // default is magazine(type:1)
+	AssetId    int `gorm:"primary_key" json:"id"`
+	AssetType  int `gorm:"default:1" json:"type"` // default is magazine(type:1)
 	MagazineId int `gorm:"type:bigint REFERENCES magazines(magazine_id)" json:"-"`
+}
+
+var AssetType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Asset",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"type": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
+
+var AssetsField = graphql.Fields{
+	"assets": &graphql.Field{
+		Type: graphql.NewList(AssetType),
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+			"type": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			var assets []Asset
+			db.DBCon.Select([]string{"asset_id, asset_type"}).Find(&assets)
+			return assets, nil
+		},
+	},
 }
