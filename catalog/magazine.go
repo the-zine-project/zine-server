f65879317014f30c21c6a887b8685da05ef8f2b6
Tanay Soni <tanaysoni12@gmail.com>
2018-04-27 21:23:02 +0530

    add cover image field

--- catalog/magazine.go
+++ catalog/magazine.go
@@ -1,13 +1,13 @@
 package catalog
 
 type Magazine struct {
-	MagazineId  int    `gorm:"primary_key"`
-	Title       string `gorm:"size:128"`
-	Description string `gorm:"size:2048"`
-	IssueNumber int
-	IssueTitle  string `gorm:"size:128"`
-	Cost        int
-	// coverImage
+	MagazineId    int    `gorm:"primary_key"`
+	Title         string `gorm:"size:128"`
+	Description   string `gorm:"size:2048"`
+	IssueNumber   int
+	IssueTitle    string `gorm:"size:128"`
+	Cost          int
+	CoverImageURL string `gorm:"size:1024"`
 
 	PublicationId int `gorm:"type:bigint REFERENCES publications(publication_id)"`
 }
