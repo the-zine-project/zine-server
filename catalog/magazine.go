8b1f448d5cf6e20b4b4ed0ded46c3fac69e5c3ae
Tanay Soni <tanaysoni12@gmail.com>
2018-04-27 22:37:29 +0530

    add graphql

--- catalog/magazine.go
+++ catalog/magazine.go
@@ -1,7 +1,12 @@
 package catalog
 
+import (
+	"github.com/graphql-go/graphql"
+	"zine/db"
+)
+
 type Magazine struct {
-	MagazineId    int    `gorm:"primary_key"`
+	MagazineId    int    `gorm:"primary_key" json:"id"`
 	Title         string `gorm:"size:128"`
 	Description   string `gorm:"size:2048"`
 	IssueNumber   int
@@ -10,4 +15,39 @@ type Magazine struct {
 	CoverImageURL string `gorm:"size:1024"`
 
 	PublicationId int `gorm:"type:bigint REFERENCES publications(publication_id)"`
+}
+
+var MagazineType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Magazine",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"issueNumber": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
+)
+
+var MagazinesField = graphql.Field{
+	Type: graphql.NewList(MagazineType),
+	Args: graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.Int,
+		},
+	},
+	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		var magazines []Magazine
+		if id, ok := p.Args["id"]; ok {
+			db.DBCon.Where(&Magazine{MagazineId: id.(int)}).Find(&magazines)
+		} else {
+			db.DBCon.Find(&magazines)
+		}
+		return magazines, nil
+	},
 }
